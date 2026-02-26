68c952d9d79610ed713a894df70087fc9e79c08a
Mannan Ali <alimannan912@gmail.com>
2026-02-26 19:14:55 +0500

    initial commit.

--- TestProject/DTOs/RegistrationDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestProject.DTOs
-{
-    public record Step1Request(string ICNumber, string FullName, string Mobile, string Email, string UserType);
-
-    public record VerifyOtpRequest(string ICNumber, string Otp, string Type);
- 
-    public record PinRequest(string ICNumber, string Pin, string ConfirmPin);
-}
