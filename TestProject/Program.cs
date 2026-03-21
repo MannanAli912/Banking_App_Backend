315aaed2e0e2f0809591042a0b34513eccabbe35
Mannan Ali <alimannan912@gmail.com>
2026-03-21 18:23:12 +0500

    updated.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();  
-
  
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
